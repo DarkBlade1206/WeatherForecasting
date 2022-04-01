97925b20f9b015cc0c55bc1fb213247c6eef167a
DarkBlade1206 <102748467+DarkBlade1206@users.noreply.github.com>
2022-04-01 17:23:51 +0530

    wrote a hello mf line

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 
 import SevenWeek from './Daily7Week'
 
-ReactDOM.render(<div><SevenWeek />
+ReactDOM.render(<div><h1>hELLO MF></h1>
 </div>, document.getElementById("root"));
